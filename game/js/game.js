c5512b20249237fdf2311a7150dae92c06aa2f18
Udo Schroeter <udo.schroeter@gmail.com>
2018-04-20 02:16:37 +0200

    added example for three.js

--- game/js/game.js
+++ game/js/game.js
@@ -1,6 +1,10 @@
 var Game = {
   
+  stage : false,
+  
   init : function(stage, frame_info_element) {
+    
+    Game.stage = stage;
     
     stage.on('mousedown_right', function(m) {
       stage.root.dragStart();
@@ -16,39 +20,39 @@ var Game = {
     });
     
     stage.on('frameinfo', function(inf) { 
-      frame_info_element.text(inf.fps+'FPS');      
+      frame_info_element.text(inf.fps+'FPS '+stage.root.children.length);      
     })
     
-    console.log('Pan stage with right mouse button');
-
     Game.createDemoObject();
   },
   
   createDemoObject : function() {
+    var scene = Game.stage.root;
+    Game.stage.renderer.setClearColor( 0x000033, 1 );
     
-    // http://pixijs.download/dev/docs/index.html
-    
-    var r = new PIXI.Graphics();
-    r.beginFill(0xffffff, 1.0);
-    r.drawRect(-50, -50, 100, 100);
-    r.position.x = 0;
-    r.position.y = 0;
-    Stage.root.addChild(r);
+    var lights = [];
+    lights[ 0 ] = new THREE.PointLight( 0xaaffff, 1, 0 );
+    lights[ 1 ] = new THREE.PointLight( 0xffaaff, 1, 0 );
+    lights[ 2 ] = new THREE.PointLight( 0xffffaa, 1, 0 );
 
-    Stage.animate(function(deltaTime) {
-      r.rotation += -deltaTime;
-      return(true);
-    });
+    lights[ 0 ].position.set( 0, 200, 0 );
+    lights[ 1 ].position.set( 100, 200, 100 );
+    lights[ 2 ].position.set( - 100, - 200, - 100 );
 
-    var t = new PIXI.Text('Hello World',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
-    t.position.x = 0;
-    t.position.y = 0;
-    Stage.root.addChild(t);
+    scene.add( lights[ 0 ] );
+    scene.add( lights[ 1 ] );
+    scene.add( lights[ 2 ] );
     
-    Stage.animation.add(function(deltaTime) {
-      t.rotation += deltaTime;
-    });
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add(cube);
+    Game.stage.camera.position.z = 1.5;
     
+    Game.stage.animate(function(dt) {
+      cube.rotation.x += 0.005;
+      cube.rotation.y += 0.005;
+      });
   },
   
 }
